362ef7206a29d541bc9e004e65e8528977ddcd52
FredoVz <dm.fredo018@gmail.com>
2024-12-13 16:54:54 +0700

    first commit

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -15,24 +15,56 @@ class Login extends CI_Controller {
         //if ($this->session->userdata('logged_in')) {
             //redirect('dashboard');
         //}
+
+        $arrayUser = [
+            [
+                "Username" => "admin",
+                "Password" => "sukses",
+            ],
+        ];
         
 		// Jika form disubmit, lakukan validasi
         if ($this->input->post()) {
             $username = $this->input->post('username', true);
             $password = $this->input->post('password', true);
 
-            // Validasi hardcoded
-            if ($username === 'admin' && $password === 'sukses') {
-                $username1 = strtolower($username);
-                // Redirect ke halaman dashboard jika berhasil login
-                // Set session userdata
-                $data = [
-                    'username_pusatmusik_backoffice' => $username1,
-                    //'logged_in' => true // Menandakan user telah login
-                ];
+            $foundUser = null;
+			foreach ($arrayUser as $user) {
+				if (strtolower($user['Username']) === strtolower($username) && $user['Password'] === $password) {
+					$foundUser = $user;
+					break;
+				}
+			}
 
-                $this->session->set_userdata($data);
-                redirect('importdigital');
+            //$username1 = $user['Username'];
+            $kataSandi = $user['Password'];
+
+            //$username === 'admin' && $password === 'sukses'
+
+            // Validasi hardcoded
+            if ($foundUser) {
+                if($password == $kataSandi) {
+                    $username1 = strtolower($username);
+                    // Redirect ke halaman dashboard jika berhasil login
+                    // Set session userdata
+                    $data = [
+                        'username_pusatmusik_backoffice' => $username1,
+                        //'logged_in' => true // Menandakan user telah login
+                    ];
+
+                    $this->session->set_userdata($data);
+                    redirect('importdigital');
+                }
+
+                else {
+                    $this->session->set_flashdata('error', [
+						'icon' => 'error',
+						'title' => 'Login Gagal!',
+						'text' => 'Password salah!',
+					]);
+					redirect('login');
+                }
+                
             } else {
                 // Set flashdata untuk pesan error
                 //$this->session->set_flashdata('error', 'Username atau Password salah!');
